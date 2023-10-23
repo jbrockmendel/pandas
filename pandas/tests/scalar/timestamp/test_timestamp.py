cf3da478749db4ce38baeab7bb4913b67e8ca015
Brock <jbrockmendel@gmail.com>
2023-10-23 16:40:03 -0700

    organize Timestamp tests

--- pandas/tests/scalar/timestamp/test_timestamp.py
+++ pandas/tests/scalar/timestamp/test_timestamp.py
@@ -30,7 +30,6 @@ from pandas._libs.tslibs.timezones import (
     tz_compare,
 )
 from pandas.compat import IS64
-import pandas.util._test_decorators as td
 
 from pandas import (
     NaT,
@@ -558,73 +557,6 @@ class TestTimestampConversion:
         assert result == expected
         assert type(result) == type(expected)
         assert result.dtype == expected.dtype
-
-    def test_to_pydatetime_fold(self):
-        # GH#45087
-        tzstr = "dateutil/usr/share/zoneinfo/America/Chicago"
-        ts = Timestamp(year=2013, month=11, day=3, hour=1, minute=0, fold=1, tz=tzstr)
-        dt = ts.to_pydatetime()
-        assert dt.fold == 1
-
-    def test_to_pydatetime_nonzero_nano(self):
-        ts = Timestamp("2011-01-01 9:00:00.123456789")
-
-        # Warn the user of data loss (nanoseconds).
-        with tm.assert_produces_warning(UserWarning):
-            expected = datetime(2011, 1, 1, 9, 0, 0, 123456)
-            result = ts.to_pydatetime()
-            assert result == expected
-
-    def test_timestamp_to_datetime(self):
-        stamp = Timestamp("20090415", tz="US/Eastern")
-        dtval = stamp.to_pydatetime()
-        assert stamp == dtval
-        assert stamp.tzinfo == dtval.tzinfo
-
-    def test_timestamp_to_datetime_dateutil(self):
-        stamp = Timestamp("20090415", tz="dateutil/US/Eastern")
-        dtval = stamp.to_pydatetime()
-        assert stamp == dtval
-        assert stamp.tzinfo == dtval.tzinfo
-
-    def test_timestamp_to_datetime_explicit_pytz(self):
-        stamp = Timestamp("20090415", tz=pytz.timezone("US/Eastern"))
-        dtval = stamp.to_pydatetime()
-        assert stamp == dtval
-        assert stamp.tzinfo == dtval.tzinfo
-
-    @td.skip_if_windows
-    def test_timestamp_to_datetime_explicit_dateutil(self):
-        stamp = Timestamp("20090415", tz=gettz("US/Eastern"))
-        dtval = stamp.to_pydatetime()
-        assert stamp == dtval
-        assert stamp.tzinfo == dtval.tzinfo
-
-    def test_to_datetime_bijective(self):
-        # Ensure that converting to datetime and back only loses precision
-        # by going from nanoseconds to microseconds.
-        exp_warning = None if Timestamp.max.nanosecond == 0 else UserWarning
-        with tm.assert_produces_warning(exp_warning):
-            pydt_max = Timestamp.max.to_pydatetime()
-
-        assert (
-            Timestamp(pydt_max).as_unit("ns")._value / 1000
-            == Timestamp.max._value / 1000
-        )
-
-        exp_warning = None if Timestamp.min.nanosecond == 0 else UserWarning
-        with tm.assert_produces_warning(exp_warning):
-            pydt_min = Timestamp.min.to_pydatetime()
-
-        # The next assertion can be enabled once GH#39221 is merged
-        #  assert pydt_min < Timestamp.min  # this is bc nanos are dropped
-        tdus = timedelta(microseconds=1)
-        assert pydt_min + tdus > Timestamp.min
-
-        assert (
-            Timestamp(pydt_min + tdus).as_unit("ns")._value / 1000
-            == Timestamp.min._value / 1000
-        )
 
     def test_to_period_tz_warning(self):
         # GH#21333 make sure a warning is issued when timezone
